359ed0d59e9082d0453b69ed35f74e7212ce1a27
ZongziTEK <stbbrd@outlook.com>
2026-01-09 22:19:03 +0800

    添加课程表同步服务（仅实现事件监听，暂未实现发送课表功能）

--- ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
+++ ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ZongziTEK_Blackboard_Sticker_Connector.Helpers;
 using ZongziTEK_Blackboard_Sticker_Connector.Models;
+using ZongziTEK_Blackboard_Sticker_Connector.Services;
 using ZongziTEK_Blackboard_Sticker_Connector.Views.Pages;
 
 namespace ZongziTEK_Blackboard_Sticker_Connector
@@ -21,6 +22,8 @@ namespace ZongziTEK_Blackboard_Sticker_Connector
         public override void Initialize(HostBuilderContext context, IServiceCollection services)
         {
             ConsoleHelper.WriteZongziTEK();
+
+            // Load and save config
             ConsoleHelper.WriteLog("开始加载配置文件", "info");
             Settings = ConfigureFileHelper.LoadConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"));  // 加载配置文件
             ConsoleHelper.WriteLog("成功加载配置文件", "info");
@@ -30,7 +33,12 @@ namespace ZongziTEK_Blackboard_Sticker_Connector
                 ConsoleHelper.WriteLog("保存配置文件", "info");
             };
 
+            // Add settings pages
             services.AddSettingsPage<SettingsPage>();
+
+            // Add services
+            services.AddHostedService<TimetableSyncService>();
+            ConsoleHelper.WriteLog("注册课程表同步服务", "info");
         }
     }
 }
