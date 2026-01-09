3140c89e1ad54770ef93ee83946661207db431f1
ZongziTEK <stbbrd@outlook.com>
2026-01-09 19:27:45 +0800

    Added TimetableHelper

---
+++ ZongziTEK_Blackboard_Sticker_Connector/Helpers/TimetableHelper.cs
@@ -0,0 +1,67 @@
+﻿using ClassIsland.Core.Abstractions.Services;
+using ClassIsland.Shared;
+using ClassIsland.Shared.Models.Profile;
+using Microsoft.Extensions.DependencyInjection;
+using ZongziTEK_Blackboard_Sticker_Connector.Models;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector.Helpers
+{
+    public class TimetableHelper
+    {
+        public static List<Timetable.Lesson> GetCurrentTimetable()
+        {
+            return GetTimetableFromClassPlan(GetCurrentClassPlan());
+        }
+
+        public static List<Timetable.Lesson> GetTimetableFromClassPlan(ClassPlan? classPlan)
+        {
+            List<Timetable.Lesson> timetable = new();
+
+            if (classPlan == null) return timetable;
+
+            var timeLayout = classPlan.TimeLayout;
+            if (timeLayout == null) return timetable;
+
+            foreach (var classInfo in classPlan.Classes)
+            {
+                Timetable.Lesson lesson = new()
+                {
+                    Subject = GetSubjectName(classInfo.SubjectId),
+                    StartTime = timeLayout.Layouts[classInfo.Index].StartTime,
+                    EndTime = timeLayout.Layouts[classInfo.Index].EndTime,
+                    IsSplitBelow = GetIsSeparatorBelow(timeLayout, classInfo.CurrentTimeLayoutItem)
+                };
+                timetable.Add(lesson);
+            }
+
+            return timetable;
+        }
+
+        public static ClassPlan? GetCurrentClassPlan()
+        {
+            var lessonsService = IAppHost.GetService<ILessonsService>();
+            return lessonsService.CurrentClassPlan;
+        }
+
+        public static string GetSubjectName(Guid subjectId)
+        {
+            var profileService = IAppHost.GetService<IProfileService>();
+            var subjects = profileService.Profile.Subjects;
+
+            subjects.TryGetValue(subjectId, out var subject);
+
+            return subject?.Name ?? string.Empty;
+        }
+
+        public static bool GetIsSeparatorBelow(TimeLayout timeLayout, TimeLayoutItem currentItem)
+        {
+            var index = timeLayout.Layouts.IndexOf(currentItem);
+            if (index == -1 || index >= timeLayout.Layouts.Count - 1)
+            {
+                return false;
+            }
+
+            return timeLayout.Layouts[index + 1].TimeType == 2;
+        }
+    }
+}
---
+++ ZongziTEK_Blackboard_Sticker_Connector/Models/Timetable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector.Models
+{
+    public class Timetable
+    {
+        public class Lesson
+        {
+            public string Subject { get; set; } = string.Empty;
+            public TimeSpan StartTime { get; set; } = TimeSpan.Zero;
+            public TimeSpan EndTime { get; set; } = TimeSpan.Zero;
+            public bool IsSplitBelow { get; set; } = false;
+            public bool IsStrongClassOverNotificationEnabled { get; set; } = false;
+        }
+    }
+}
