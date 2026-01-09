173151022574a312fa2eaa693e313bc47e6ef857
ZongziTEK <stbbrd@outlook.com>
2026-01-09 16:39:39 +0800

    Added log system

---
+++ ZongziTEK_Blackboard_Sticker_Connector/Helpers/ConsoleHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector.Helpers
+{
+    public class ConsoleHelper
+    {
+        public static void WriteLog(string message, string level)
+        {
+            // Timestamp
+            var timestamp = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            Console.Write($"{timestamp} | ");
+
+            // Level
+            var levelColor = level.ToLower() switch
+            {
+                "info" => ConsoleColor.Green,
+                "warning" => ConsoleColor.Yellow,
+                "error" => ConsoleColor.Red,
+                "debug" => ConsoleColor.Gray,
+                _ => ConsoleColor.White
+            };
+
+            if (levelColor != ConsoleColor.White)
+            {
+                Console.ForegroundColor = levelColor;
+                Console.Write(level);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write(level);
+            }
+
+            Console.Write(" | ");
+
+            // Source
+            var callerTypeName = new StackTrace(1).GetFrame(0)?.GetMethod()?.DeclaringType?.FullName ?? "Unknown";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(callerTypeName);
+            Console.ResetColor();
+
+            // Message
+            Console.WriteLine($" | {message}");
+        }
+
+        public static void WriteZongziTEK()
+        {
+            string[] zongzitek = {
+            @" ________  ________  ________   ________  ________  ___  _________  _______   ___  __",
+            @"|\_____  \|\   __  \|\   ___  \|\   ____\|\_____  \|\  \|\___   ___\\  ___ \ |\  \|\  \",
+            @" \|___/  /\ \  \|\  \ \  \\ \  \ \  \___| \|___/  /\ \  \|___ \  \_\ \   __/|\ \  \/  /|_",
+            @"     /  / /\ \  \\\  \ \  \\ \  \ \  \  ___   /  / /\ \  \   \ \  \ \ \  \_|/_\ \   ___  \",
+            @"    /  /_/__\ \  \\\  \ \  \\ \  \ \  \|\  \ /  /_/__\ \  \   \ \  \ \ \  \_|\ \ \  \\ \  \",
+            @"   |\________\ \_______\ \__\\ \__\ \_______\\________\ \__\   \ \__\ \ \_______\ \__\\ \__\",
+            @"    \|_______|\|_______|\|__| \|__|\|_______|\|_______|\|__|    \|__|  \|_______|\|__| \|__|"
+            };
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (var line in zongzitek)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ResetColor();
+        }
+    }
+}
--- ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
+++ ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
@@ -7,6 +7,7 @@ using ClassIsland.Shared.Helpers;
 using Iced.Intel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ZongziTEK_Blackboard_Sticker_Connector.Helpers;
 using ZongziTEK_Blackboard_Sticker_Connector.Models;
 using ZongziTEK_Blackboard_Sticker_Connector.Views.Pages;
 
@@ -19,10 +20,14 @@ namespace ZongziTEK_Blackboard_Sticker_Connector
 
         public override void Initialize(HostBuilderContext context, IServiceCollection services)
         {
+            ConsoleHelper.WriteZongziTEK();
+            ConsoleHelper.WriteLog("开始加载配置文件", "info");
             Settings = ConfigureFileHelper.LoadConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"));  // 加载配置文件
+            ConsoleHelper.WriteLog("成功加载配置文件", "info");
             Settings.PropertyChanged += (sender, args) =>
             {
                 ConfigureFileHelper.SaveConfig(Path.Combine(PluginConfigFolder, "Settings.json"), Settings);  // 保存配置文件
+                ConsoleHelper.WriteLog("保存配置文件", "info");
             };
 
             services.AddSettingsPage<SettingsPage>();
