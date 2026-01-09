38eaf0b37da2becc4fe8fedeac803a691dd5d8a2
ZongziTEK <stbbrd@outlook.com>
2026-01-09 14:17:38 +0800

    Added "Settings" model

---
+++ ZongziTEK_Blackboard_Sticker_Connector/Models/Settings.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector.Models
+{
+    public class Settings : ObservableRecipient
+    {
+        private bool _isTimetableShared = true;
+        private bool _isClassIslandAutoHideEnabled = true;
+
+        public bool IsTimetableShared
+        {
+            get => _isTimetableShared;
+            set
+            {
+                if (value == _isTimetableShared) return;
+                _isTimetableShared = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsClassIslandAutoHideEnabled
+        {
+            get => _isClassIslandAutoHideEnabled;
+            set
+            {
+                if (value == _isClassIslandAutoHideEnabled) return;
+                _isClassIslandAutoHideEnabled = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
--- ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
+++ ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
@@ -2,18 +2,26 @@
 using ClassIsland.Core.Abstractions;
 using ClassIsland.Core.Attributes;
 using ClassIsland.Core.Controls;
+using ClassIsland.Core.Extensions.Registry;
+using ClassIsland.Shared.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ZongziTEK_Blackboard_Sticker_Connector.Models;
 
 namespace ZongziTEK_Blackboard_Sticker_Connector
 {
     [PluginEntrance]
     public class Plugin : PluginBase
     {
+        public Settings Settings { get; set; } = new();
+
         public override void Initialize(HostBuilderContext context, IServiceCollection services)
         {
-            AppBase.Current.AppStarted += async (_, _) =>
-                await CommonTaskDialogs.ShowDialog("Hello world!", "Hello from ZongziTEK_Blackboard_Sticker_Connector!");
+            Settings = ConfigureFileHelper.LoadConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"));  // 加载配置文件
+            Settings.PropertyChanged += (sender, args) =>
+            {
+                ConfigureFileHelper.SaveConfig<Settings>(Path.Combine(PluginConfigFolder, "Settings.json"), Settings);  // 保存配置文件
+            };
         }
     }
 }
