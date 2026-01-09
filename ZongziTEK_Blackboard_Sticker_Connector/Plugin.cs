9080ec903834eae96dbcc5411ce1bc0e2e1c75b4
ZongziTEK <stbbrd@outlook.com>
2026-01-09 14:04:36 +0800

    添加项目文件。

---
+++ ZongziTEK_Blackboard_Sticker_Connector/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using ClassIsland.Core;
+using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Attributes;
+using ClassIsland.Core.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector
+{
+    [PluginEntrance]
+    public class Plugin : PluginBase
+    {
+        public override void Initialize(HostBuilderContext context, IServiceCollection services)
+        {
+            AppBase.Current.AppStarted += async (_, _) =>
+                await CommonTaskDialogs.ShowDialog("Hello world!", "Hello from ZongziTEK_Blackboard_Sticker_Connector!");
+        }
+    }
+}
---
+++ ZongziTEK_Blackboard_Sticker_Connector/README.md
@@ -0,0 +1,14 @@
+# ZongziTEK 黑板贴连接器
+
+该插件能够让 ZongziTEK 黑板贴 与 ClassIsland 进行连接与交互。
+
+## 插件功能
+
+- 合理切换 ClassIsland 的显示状态
+- 将 ClassIsland 当前显示的课程表发送到黑板贴，并在黑板贴显示
+
+## 黑板贴是什么
+
+ZongziTEK 黑板贴是一款综合信息展示软件，常驻于桌面，用于展示作业、课程、天气等信息，并为老师提供能快捷启动应用程序的启动台。
+
+您可以在 [https://zongzitek.top](https://zongzitek.top) 下载黑板贴
