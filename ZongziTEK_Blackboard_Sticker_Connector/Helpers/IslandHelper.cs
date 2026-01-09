b7c6fae4c7ae86d4dfc10f7c1da6de1a819371fc
ZongziTEK <stbbrd@outlook.com>
2026-01-09 18:11:46 +0800

    Added IslandHelper

---
+++ ZongziTEK_Blackboard_Sticker_Connector/Helpers/IslandHelper.cs
@@ -0,0 +1,19 @@
+﻿using ClassIsland.Core;
+
+namespace ZongziTEK_Blackboard_Sticker_Connector.Helpers
+{
+    public class IslandHelper
+    {
+        public static void ShowIsland()
+        {
+            dynamic app = AppBase.Current;
+            app.Settings.IsMainWindowVisible = true;
+        }
+
+        public static void HideIsland()
+        {
+            dynamic app = AppBase.Current;
+            app.Settings.IsMainWindowVisible = false;
+        }
+    }
+}
